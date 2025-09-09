1b56537b5dd23fae3663562ea51ede41570359c5
Harry Liu <harryliu0416@gmail.com>
2025-09-09 13:23:25 -0500

    a

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,23 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /* The map interface is generic
+         * the first type given is the key
+         * the second type given is the value
+         */
+        Map<String, Color> favColor = new HashMap<>();
+
+        favColor.put("Sreeram", Color.GREEN);
+        favColor.put("Kaitlyn", Color.GREEN);
+        favColor.put("Cam", Color.BLUE);
+        favColor.put("Nimai", Color.RED);
+
+        favColor.put("Cam", Color.ORANGE);
+
+        Set<String> keys = favColor.keySet();
+
+        for (String key: keys) {
+            System.out.println(key + "(" + key.hashCode() + "): " + favColor.get(key));
+        }
     }
 }
