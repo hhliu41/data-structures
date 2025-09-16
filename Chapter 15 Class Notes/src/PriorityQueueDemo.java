70261bb35885fd4b2daf5ed89382d222aa7f822d
Harry Liu <harryliu0416@gmail.com>
2025-09-16 13:20:13 -0500

    a

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,29 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        Queue<String> students = new PriorityQueue<>();
+        students.add("Neel");
+        students.add("Jonathan");
+        students.add("Cam");
+        students.add("Kaitlyn");
+        students.add("Dylan");
+        while (!students.isEmpty()){
+            System.out.println(students.remove());
+        }
+
+        Queue<WorkOrder> toDo = new PriorityQueue<>();
+
+        toDo.add(new WorkOrder(3, "Water Plants"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1, "Conquer World"));
+        toDo.add(new WorkOrder(9, "Play Videogames"));
+        toDo.add(new WorkOrder(2, "Make Dinner"));
+        toDo.add(new WorkOrder(1, "Study for the Ch. 15 Test"));
+
+        System.out.println(toDo);
+        while (!toDo.isEmpty()){
+            System.out.println(toDo.remove());
+        }
+
     }
 }
--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,29 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        Queue<String> jobs = new LinkedList<>();
+
+        jobs.add("Harry: Quarter 2 Expense Report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Documents");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Documents");
+        jobs.add("Katherine: Can I Get Fired For This?");
+
+        System.out.println("Printing: "+jobs.remove());
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Boss: Katherine Termination Letter");
+        /*int numJobs = jobs.size();
+        for (int i = 0; i < numJobs; i++) {
+            System.out.println("Printing: "+jobs.remove());
+        }*/
+        while (!jobs.isEmpty()) {
+            System.out.println("Printing: "+jobs.remove());
+        }
+
     }
 }
