1ff5cbe380ea2c74e4b74435613ce5dc567f341e
Harry Liu <harryliu0416@gmail.com>
2025-09-30 13:26:08 -0500

    Queue and Stack Implementation

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -6,6 +6,10 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
+    private int currentSize;
+    private int head;
+    private int tail;
+
     //private data
 
 
@@ -13,7 +17,13 @@ public class CircularArrayQueue
     /**
         Constructs an empty queue.
     */
-
+    public CircularArrayQueue() {
+        final int INITIAL_SIZE = 2;
+        this.elements = new Object[INITIAL_SIZE];
+        this.head = 0;
+        this.tail = 0;
+        this.currentSize = 0;
+    }
 
 
 
@@ -23,6 +33,9 @@ public class CircularArrayQueue
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
+    public boolean empty() {
+        return this.currentSize == 0;
+    }
 
 
 
@@ -31,6 +44,14 @@ public class CircularArrayQueue
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
+    public void add(Object element) {
+        this.currentSize++;
+        this.elements[this.tail] = element;
+        this.tail++;
+        // Reset tail to 0 when it reaches the size of the array
+        this.tail %= this.elements.length;
+        growIfNecessary();
+    }
 
 
 
@@ -40,7 +61,16 @@ public class CircularArrayQueue
         Removes an element from the head of this queue.
         @return the removed element
     */
+    public Object remove() {
+        if (this.empty()) {
+            throw new NoSuchElementException();
+        }
 
+        this.currentSize--;
+        Object element = this.elements[this.head];
+        this.head = (this.head+1) % this.elements.length;
+        return element;
+    }
 
 
 
@@ -50,7 +80,7 @@ public class CircularArrayQueue
     */
     private void growIfNecessary()
     {
-        /*
+        
         if(this.currentSize == this.elements.length)
         {
             Object[] newElements = new Object[2 * this.elements.length];
@@ -62,7 +92,7 @@ public class CircularArrayQueue
             this.head = 0;
             this.tail = this.currentSize;
         }
-        */
+        
     }
 
 
